1d2a7a8ee84d6d294a75af831caa5385ad1491ae
Cedric Lang-Roth <dnj855@yahoo.com>
2024-12-03 16:32:51 +0100

    Add artworks#new action and modified artworks validation + setup cloudinary

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -3,4 +3,14 @@ class ArtworksController < ApplicationController
     @artwork = Artwork.find(params[:id])
     @booking = Booking.new
   end
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :price_by_day, :object_date, :dimensions, :artist_id)
+  end
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,8 +2,8 @@ class Artwork < ApplicationRecord
   belongs_to :artist
   belongs_to :owner, class_name: "User"
   has_many :bookings
+  has_one_attached :image
 
   validates :title, presence: true
-  validates :image_url, presence: true
   validates :price_by_day, presence: true, numericality: { only_numeric: true, greater_than: 50 }
 end
