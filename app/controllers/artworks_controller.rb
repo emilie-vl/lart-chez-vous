c68f584bce3780aaa28974cc3aa7c1d94802c371
Guillaume Fournier <fournierg01@gmail.com>
2024-12-03 15:43:15 +0100

    update

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,6 +1,6 @@
 class ArtworksController < ApplicationController
   def index
-    @artworks = Artwork.where(title: params[:query])
+    @artworks = Artwork.where(title: "La nuit étoilée")         #where(title: params[:query])
   end
   def show
     @artwork = Artwork.find(params[:id])
