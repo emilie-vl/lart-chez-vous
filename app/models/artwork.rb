016d5a0786fb585dac750a43c2a15ca041c46d0c
Cedric Lang-Roth <dnj855@yahoo.com>
2024-12-03 11:20:02 +0100

    Validation of artworks

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,9 @@
 class Artwork < ApplicationRecord
   belongs_to :artist
   belongs_to :owner, class_name: "User"
+
+  validates :title, presence: true
+  validates :image_url, presence: true
+  validates :price_by_day, presence: true, numericality: { only_numeric: true, greater_than: 50 }
+  validates :classification, presence: true
 end
