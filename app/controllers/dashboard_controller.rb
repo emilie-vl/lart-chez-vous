e3b7608e30391684ed30799d207f0e478bcb4ba1
Helene Demanet <demanet.helene@gmail.com>
2024-12-04 22:56:04 +0100

    add delete artwork, not finish

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,4 +6,17 @@ class DashboardController < ApplicationController
   @coming_rentals = Booking.where("renter_id = ? and begin_date >= ?", current_user.id, Date.today)
   @bookings = Booking.joins(:artwork).where(artworks: { owner_id: current_user.id })
   end
+
+  def destroy_artwork
+    @artwork = Artwork.find(params[:id])
+
+    if @artwork.bookings.exists?
+      redirect_to dashboard_path(anchor: "myArtworks"), alert: "Vous ne pouvez pas supprimer cette œuvre car elle est liée à des réservations.", status: :see_other
+    elsif @artwork.owner_id == current_user.id
+      @artwork.destroy
+      redirect_to dashboard_path(anchor: "myArtworks"), notice: "Votre œuvre a bien été supprimée.", status: :see_other
+    else
+      redirect_to dashboard_path(anchor: "myArtworks"), alert: "Vous ne pouvez pas supprimer cette œuvre.", status: :see_other
+    end
+  end
 end
