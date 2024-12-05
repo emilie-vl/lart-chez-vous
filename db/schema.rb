f77598f7f00fbbacb96b5a7b91ee85245a4bca45
Cedric Lang-Roth <dnj855@yahoo.com>
2024-12-05 09:44:16 +0100

    Enhance the bookings validation process

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_151517) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_05_082426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_151517) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "validated", default: false
+    t.boolean "processed", default: false
     t.index ["artwork_id"], name: "index_bookings_on_artwork_id"
     t.index ["renter_id"], name: "index_bookings_on_renter_id"
   end
